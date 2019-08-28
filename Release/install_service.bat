d45b4cd9ae358ab3db83ce030b050a7d3ea59c6c
martrinex <54643832+martrinex@users.noreply.github.com>
2019-08-28 20:43:03 +0100

    Add files via upload

---
+++ Release/install_service.bat
@@ -0,0 +1,4 @@
+netsh http add urlacl url=https://+:443/deviceonline user=system
+netsh http add urlacl url=http://+:80/deviceonline user=system
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe deviceonline.exe
+pause
